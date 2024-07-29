004e727cc68816e986fbd2c47a544e21f373bc91
Summer H <sh.hardison.sh@icloud.com>
2024-07-29 15:40:56 -0400

    Progress with seeding plus added routes

--- models/index.js
+++ models/index.js
@@ -2,18 +2,9 @@ const User = require("./user");
 const Post = require("./post");
 const Comment = require("./post");
 
-User.hasMany(Post, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
@@ -21,12 +12,9 @@ Post.hasMany(Comment, {
     onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 module.exports = { User, Post, Comment };
