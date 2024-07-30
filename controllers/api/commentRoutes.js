6bc6006a559320c5e087f4a9474ac8622022abac
Summer H <sh.hardison.sh@icloud.com>
2024-07-30 09:58:43 -0400

    Debugs comments and completes seeding, adds new routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, User, Post } = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
-        const commentData = await Comment.findAll();
+        const commentData = await Comment.findAll({
+            include: [{model: User}, {model: Post}]
+        });
         res.status(200).json(commentData);
     } catch (err) {
         res.status(500).json(err);
@@ -31,7 +33,7 @@ router.delete("/:id", async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const commentData = await Comment.create(req.body);
-        res.status(200).json(commentData)
+        res.status(200).json(commentData);
     } catch (err) {
         res.status(500).json(err);
     }
