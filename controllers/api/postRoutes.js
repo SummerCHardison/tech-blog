5498aaefb556d522d9f0cf85a390bf96fb50bf24
Summer H <sh.hardison.sh@icloud.com>
2024-07-30 14:42:58 -0400

    Displays some unstyled info

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,14 +15,25 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
-            include: [{model: User}, {model: Comment}]
+            include: [
+                {
+                    model: User,
+                    attributes: ['user_name']
+                },
+                {
+                    model: Comment,
+                    attributes: ['comment_body']
+                }
+            ]
         });
 
         if(!postData) {
             res.status(404).json({message: 'No post found with this id!'});
         };
 
-        res.status(200).json(postData);
+        res.render('post', {
+            postData
+        });
     } catch (err) {
         res.status(500).json(err);
     }
