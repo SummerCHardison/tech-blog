4731133ae3258b3368f70f78f257e689b9ec9094
Summer H <sh.hardison.sh@icloud.com>
2024-07-30 12:27:25 -0400

    Adds routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -10,4 +10,28 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id);
+
+        if(!userData) {
+            res.status(404).json({message: 'No user found with this id!'});
+        };
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    res.status
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
